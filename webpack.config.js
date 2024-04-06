5e43d2ca695023b6c00bba0bc6bd7f3a4cedaf3e
Shashi <ranjanshashi4862@gmail.com>
2024-04-06 16:36:15 +0530

    first

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+export default defineConfig({
+    base: "/Stakepool-Frontend/",
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "node_modules"),
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
+});
